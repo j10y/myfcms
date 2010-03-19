75747dc8d926e0331e71b1e65b8b398474deafbc
yourfei@live.cn <yourfei@live.cn@c1c2bf6e-ae9d-b823-7fc2-ff1dc594729e>
2010-03-19 01:35:13 +0000

    添加hashCode和equals方法

--- trunk/hdemo/src/base/user/model/BaseUser.java
+++ trunk/hdemo/src/base/user/model/BaseUser.java
@@ -225,4 +225,37 @@ public class BaseUser implements Serializable {
 	public void setRoles(Set<Role> roles) {
 		this.roles = roles;
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final BaseUser other = (BaseUser) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 }
