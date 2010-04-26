b820e0e046f4509c5fe7b577326b5f9a585b15b2
yourfei <yourfei@live.cn>
2010-04-26 03:35:36 +0000

    宽度调整

---
+++ zkdemo/src/com/bdzb/oa/member/model/Member.java
@@ -0,0 +1,19 @@
+/**
+ * <p>项目名称：</p>
+ * <p>版权所有 (c) </p>
+ * <p>版本：1.0</p>
+ * <p>日期：Apr 23, 2010</p>
+ * <p>更新：</p>
+ */
+package com.bdzb.oa.member.model;
+
+/**
+ * @author xiacc
+ *
+ * 描述：商业会员类
+ */
+public class Member {
+	
+	private Long id;
+
+}
