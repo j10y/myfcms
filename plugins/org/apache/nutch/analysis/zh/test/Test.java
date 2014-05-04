fe518a6b07af5074dc19b02f083564dcb3182310
xiacc1984 <xiacc1984@gmail.com>
2014-05-04 03:24:51 +0000

    更换为ansj分词

---
+++ plugins/org/apache/nutch/analysis/zh/test/Test.java
@@ -0,0 +1,47 @@
+package org.apache.nutch.analysis.zh.test;
+
+import java.io.StringReader;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.TermAttribute;
+import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
+import org.apache.nutch.analysis.zh.ChineseAnalyzer;
+import org.apache.nutch.util.NutchConfiguration;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/* 标准分词器：单子分词 */
+		try {
+			String text2 = "党政 领导 干部 选拔 任用 工作 条例";
+			Configuration conf = NutchConfiguration.createCrawlConfiguration();
+			Analyzer analyzer = new ChineseAnalyzer();
+			testAnalyzer(analyzer, text2); // 使用IKAnalyzer，词库分词
+	
+//			System.out.println(new ToAnalysis().parse("党政领导干部选拔任用工作条例")); 
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+	}
+
+	private static void testAnalyzer(Analyzer analyzer, String text) throws Exception {
+		System.out.println("当前使用的分词器：" + analyzer.getClass());
+
+		TokenStream tokenStream = analyzer.tokenStream("DEFAULT", new StringReader(text));
+		tokenStream.addAttribute(TypeAttribute.class);
+
+		while (tokenStream.incrementToken()) {
+			TermAttribute termAttribute = tokenStream.getAttribute(TermAttribute.class);
+			System.out.print(termAttribute.term()+",");
+		}
+	}
+
+}
